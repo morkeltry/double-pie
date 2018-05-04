278230dbda094d8bbcb9372ce62524a29f2e8526
malcolm twhoo <morkeltry@mailfence.com>
2018-05-04 15:44:13 +0100

    arcs sorted. Arcs now update with .exit, .enter

--- app.js
+++ app.js
@@ -5,43 +5,68 @@ const outerRadius = 350;
 const innerRadius = 100;
 const translation = `translate (${outerRadius+100},${outerRadius+100})`;
 
-
+const keyByMonth = (d,idx) => {
+  let months = ['','January', 'February', 'March', 'April', 'May', 'June',
+    'July', 'August', 'September', 'October', 'November', 'December']
+  let month = months.indexOf(d.month);
+  let dateNumerical = d.year*100+month
+  console.log ('dateNumerical',dateNumerical);
+  console.log ('data',d);
+  return dateNumerical;
+  };
 
 const doUpdate = () => {
-  console.log (d3.event.target.value);
+  const inputValue = d3.event ? d3.event.target.value : '1967';
+  console.log ('inputValue',inputValue);
 
   const thisYearData = birthData.filter( d=>
-    d.year == d3.event.target.value
+    d.year == inputValue
   );
+  if (!Object.keys(thisYearData).length)
+    console.log ('Warning! thisYearData is empty');
 
   const arcs = d3.pie()
     .value (d => d.births)
+    .sort ((a,b) => keyByMonth(a)-keyByMonth(b))
     (thisYearData);
-  console.log(arcs);
+
+  console.log('arcs:',arcs);
 
   const path = d3.arc()
     .outerRadius(outerRadius)
     .innerRadius(innerRadius);
-  console.log(path);
+  console.log('path from arc:',path);
+
+  const fill = (d,i) =>
+    (d.data.births)%2? '#19cccc'
+    : '#e54c85'
 
   const piePaths = d3.select ("#pie")
       .selectAll("path")
-        // .data (thisYearData);
 
-  // piePaths
-  //   .data (arcs)
-  //   .exit()
-  //   .remove();
+  console.log('paths:',piePaths);
+  console.log('updated:',  piePaths
+    .data (arcs,keyByMonth));
+  console.log('exit:',  piePaths
+    .data (arcs,keyByMonth)
+    .exit());
+  piePaths
+    .data (arcs,keyByMonth)
+    .exit()
+    .remove();
+  console.log('paths again:',piePaths);
 
+// if (d3.event == null)
   piePaths
     .data (arcs)
     .enter()
     .append ("path")
       .attr ("stroke-fill",'blue')
       .attr ("transform",translation)
+      .attr ("fill", fill)
       .attr ("d",path)
 
-  console.log(piePaths);
+  console.log('paths again again:',piePaths);
 }
 
 
@@ -51,3 +76,5 @@ d3.select("input")
     .property("value", minYear)
     .on("change", doUpdate)
 ;
+
+doUpdate();
