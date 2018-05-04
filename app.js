bb46e4dbd6f515d7f51a6ba9456aed104ce8e74d
malcolm twhoo <morkeltry@mailfence.com>
2018-05-05 00:03:27 +0100

    added second data structure for inner pie and coloured it garish

--- app.js
+++ app.js
@@ -27,25 +27,49 @@ const doUpdate = () => {
   if (!Object.keys(thisYearData).length)
     console.log ('Warning! thisYearData is empty');
 
+  const thisYearsQuarterly = [0,0,0,0];
+  thisYearData.forEach (oneMonthsData => {
+    thisYearsQuarterly [ Math.floor(((keyByMonth(oneMonthsData)-1)%100)/3) ] += oneMonthsData.births;
+  });
+
   const arcs = d3.pie()
     .value (d => d.births)
     .sort ((a,b) => keyByMonth(a)-keyByMonth(b))
     (thisYearData);
 
+
+  // setting both data comparator (with .sort) and value comparator (with .sortValues)
+  // seems a long way round to tell d3.pie we don;t want them sorted. Is there a quicker way?
+  const qtlyArcs = d3.pie()
+    .sortValues (null)
+    .sort (null)
+    (thisYearsQuarterly);
+
   const path = d3.arc()
     .outerRadius(outerRadius)
     .innerRadius(innerRadius);
 
-  const fill = (d,i) =>
-    (keyByMonth(d.data))%2? '#19cccc'
-    : '#e54c85'
+  const innerPath = d3.arc()
+    .outerRadius(innerRadius)
+    .innerRadius(0);
 
-  const piePaths = d3.select ("#pie")
+  const fill = (d,i) =>
+    d.data.month?
+      (keyByMonth(d.data))%2? '#19cccc'
+        : '#e54c85'
+      : i%2? '#19cc85'
+        : '#e54ccc';
+
+  const piePaths = d3.select ("#outer-pie")
       .selectAll("path")
         .data (arcs)
 
         //This VV part of the line caused paths bloat. Why was it in there?
         // , d=>keyByMonth(d.data))
+
+  const innerPiePaths = d3.select ("#inner-pie")
+      .selectAll("path")
+        .data (qtlyArcs)
 
   piePaths
     .enter()
@@ -58,6 +82,18 @@ const doUpdate = () => {
 
     .merge (piePaths)
       .attr ("d",path);
+
+  innerPiePaths
+    .enter()
+    .append ("path")
+      .attr ("id",d=>d)
+      .attr ("stroke-fill",'blue')
+      .attr ("transform",translation)
+      .attr ("fill", fill)
+      .attr ("d",innerPath)
+
+    .merge (innerPiePaths)
+      .attr ("d", innerPath);
 }
 
 
@@ -68,4 +104,7 @@ d3.select("input")
     .on("change", doUpdate)
 ;
 
+d3.select ("#slider")
+    .attr ("transform",translation)
+    .attr ("style", 'width:'+outerRadius*1.5+'px');
 doUpdate();
